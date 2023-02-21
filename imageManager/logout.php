af09ca03939edf0f0c0d30c7689c875e755a5e25
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-02-21 17:35:40 +0530

    Update New File

--- imageManager/logout.php
+++ imageManager/logout.php
@@ -1,7 +1,7 @@
 <?php 
 
 session_start();
-unset($_SESSION['username']);
+unset($_SESSION['loggedinUser']);
 session_destroy();
 echo "<script>alert('You have been logged out')</script>";
 header("location:login.php");
